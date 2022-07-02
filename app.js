8c971375038768c2b0abdcf2e782c9d42d6e5490
Kishore Babu <polurukishorebabu@gmail.com>
2022-07-02 15:24:44 +0530

    DB connection

---
+++ app.js
@@ -0,0 +1,32 @@
+require("dotenv").config();
+
+const mongoose = require("mongoose");
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+
+//DB Connection
+mongoose
+    .connect(process.env.DATABASE, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => {
+        console.log("DB CONNECTED");
+    });
+
+//Middlewares
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors());
+
+
+//PORT
+const port = process.env.PORT || 8080;
+
+//Starting a server
+app.listen(port, () => {
+  console.log(`app is running at ${port}...`);
+});
